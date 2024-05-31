fd774cf01bfff5c09ba70f87e0bd619ccc258afb
Christopher Wong <chriswong0839@gmail.com>
2024-05-31 15:29:45 +0800

    Updated FoodOrderingModuleScreen.kt
    Changes:
    - Added Order List

--- app/src/main/java/com/example/jomdining/ui/FoodOrderingModuleScreen.kt
+++ app/src/main/java/com/example/jomdining/ui/FoodOrderingModuleScreen.kt
@@ -3,31 +3,19 @@ package com.example.jomdining.ui
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -35,17 +23,18 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.example.jomdining.R
 import com.example.jomdining.data.TempMenuItems
 import com.example.jomdining.databaseentities.Menu
-import java.io.InputStream
 import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -81,7 +70,7 @@ fun FoodOrderingModuleScreen(
                         modifier = modifier
                     )
                 }
-                SecondItem(
+                OrderSummary(
                     modifier = Modifier
                         .weight(0.4f)
                         .fillMaxHeight()
@@ -115,12 +104,7 @@ fun MenuItemCard(
 ) {
     Card(
         shape = RoundedCornerShape(8.dp),
-        modifier = modifier.padding(
-            start = 16.dp,
-            end = 16.dp,
-            top = 16.dp,
-            bottom = 16.dp
-        ),
+        modifier = modifier.padding(16.dp),
         colors = CardDefaults.cardColors(
             containerColor = Color.White,
         )
@@ -137,12 +121,11 @@ fun MenuItemCard(
                 val imagePath = menuItem.menuItemImagePath
                 Log.d("IMAGE_PATH", "Current image path is $imagePath")
                 val assetManager = LocalContext.current.assets
-                val inputStream: InputStream?
-                try {
-                    inputStream = assetManager.open(imagePath)
-                    Log.d("IMAGE_FILE", "Image file exists in assets: $inputStream")
+                val inputStream = try {
+                    assetManager.open(imagePath)
                 } catch (e: Exception) {
                     Log.e("IMAGE_FILE_ERROR", "Image file does not exist in assets: $e")
+                    null
                 }
                 Image(
                     painter = rememberAsyncImagePainter(
@@ -187,35 +170,202 @@ fun MenuItemCard(
     }
 }
 
-//@Composable
-//fun OrderItemCard(
-//    orderItem: OrderItem,
-//    modifier: Modifier = Modifier
-//) {
-//    Card {
-//        Row {
-//            Box {
-//                /*
-//                                Image {
-//                    // painter = painterResource(id = )
-//                }
-//                 */
-//            }
-//        }
-//    }
-//}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OrderSummary(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .background(Color(0xFFE6E6E6))
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Order #12345",
+            style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.Bold),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp)
+                .background(Color(0xFF34197C), shape = RoundedCornerShape(8.dp))
+                .padding(8.dp),
+            color = Color.White,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn(
+            modifier = Modifier.weight(1f) // Make it take available space and be scrollable
+        ) {
+            items(4) {
+                OrderItemCard()
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Total",
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold)
+            )
+            Text(
+                text = "RM 65.00",
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold)
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Table No.",
+                style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold)
+            )
+            TextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier
+                    .width(60.dp)
+                    .height(40.dp)
+                    .background(Color.White, shape = RoundedCornerShape(8.dp)),
+                colors = TextFieldDefaults.textFieldColors(
+                    textColor = Color.Black,
+                    containerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                )
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.Center // Center the buttons horizontally
+        ) {
+            Button(
+                onClick = { /* Place Order Action */ },
+                modifier = Modifier
+                    .width(300.dp)
+                    .height(65.dp)
+                    .padding(vertical = 8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF34197C))
+            ) {
+                Text(
+                    "Place Order",
+                    color = Color.White,
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    )
+                )
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.Center // Center the buttons horizontally
+        ) {
+            Button(
+                onClick = { /* Cancel Order Action */ },
+                modifier = Modifier
+                    .width(300.dp)
+                    .height(65.dp)
+                    .padding(vertical = 8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFDC143C))
+            ) {
+                Text(
+                    "Cancel",
+                    color = Color.White,
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    )
+                )
+            }
+        }
+    }
+}
+
+
+
 
 @Composable
-fun SecondItem(
-    modifier: Modifier
-) {
-    Box(
+fun OrderItemCard(modifier: Modifier = Modifier) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+        ),
         modifier = modifier
-            .background(Color.Black)
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
+            .fillMaxWidth()
     ) {
-        Text(text = "Second Item")
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Image(
+                painter = rememberAsyncImagePainter(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data("file:///android_asset/images/chickenChop.png")
+                        .build()
+                ),
+                contentDescription = "Ordered Item",
+                modifier = Modifier
+                    .size(64.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            )
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 8.dp)
+            ) {
+                Text("Fried Chicken", fontWeight = FontWeight.Bold)
+                Text("RM 20.00", color = Color(0xFF7C4DFF))
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(32.dp)
+                        .clip(RoundedCornerShape(4.dp))
+                        .background(Color.Red)
+                        .clickable { /* Decrease Quantity */ },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Remove,
+                        contentDescription = "Reduce Quantity",
+                        tint = Color.White
+                    )
+                }
+                Text("1", modifier = Modifier.padding(horizontal = 8.dp))
+                Box(
+                    modifier = Modifier
+                        .size(32.dp)
+                        .clip(RoundedCornerShape(4.dp))
+                        .background(Color.Green)
+                        .clickable { /* Increase Quantity */ },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Add Quantity",
+                        tint = Color.White
+                    )
+                }
+                IconButton(onClick = { /* Delete Item */ }) {
+                    Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete Item", tint = Color.Red)
+                }
+            }
+        }
     }
 }
 
@@ -260,8 +410,6 @@ fun TestMenuItemGrid(
 ) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(3),
-        // verticalArrangement = Arrangement.spacedBy(0.dp),
-        // horizontalArrangement = Arrangement.spacedBy(24.dp),
         modifier = modifier
             .background(backgroundColor)
     ) {
@@ -305,10 +453,11 @@ fun FoodOrderingModulePreview(
                 ) {
                     TestMenuItemGrid(modifier = modifier)
                 }
-                SecondItem(
+                OrderSummary(
                     modifier = Modifier
                         .weight(0.4f)
-                        .fillMaxSize()
+                        .fillMaxHeight()
+                        .background(Color(0xFFD9E6FF))
                 )
             }
         }
