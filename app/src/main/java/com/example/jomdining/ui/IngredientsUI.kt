427b499183933de96899baf1a24b6d0896d689f8
Chiew Cheng Yi <chiewccy1@gmail.com>
2024-06-05 15:26:07 +0800

    File refactoring executed
    InventoryManagementScreen.kt was renamed StockManagementScreen.kt

---
+++ app/src/main/java/com/example/jomdining/ui/IngredientsUI.kt
@@ -0,0 +1,7 @@
+package com.example.jomdining.ui
+
+import com.example.jomdining.databaseentities.MenuItemIngredient
+
+data class MenuItemIngredientsUI(
+    val MenuItemIngredientsList: List<MenuItemIngredient> = listOf()
+)
